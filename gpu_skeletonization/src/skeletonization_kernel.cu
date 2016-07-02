9fdeedee891fad0b73fc7c87b5c5304fc4b917f9
Krishneel <krishneel.chand.chaudhary@gmail.com>
2016-07-03 04:16:59 +0900

    moved absdiff to cuda

--- gpu_skeletonization/src/skeletonization_kernel.cu
+++ gpu_skeletonization/src/skeletonization_kernel.cu
@@ -22,7 +22,7 @@ int cuDivUp(int a, int b) {
 
 
 __global__
-void skeletonizationKernel(unsigned char *d_marker, unsigned char *d_image,
+void skeletonizationKernel(unsigned char *d_image,
                            int iter, const int width, const int height) {
     int t_idx = threadIdx.x + blockIdx.x * blockDim.x;
     int t_idy = threadIdx.y + blockIdx.y * blockDim.y;
@@ -52,28 +52,40 @@ void skeletonizationKernel(unsigned char *d_marker, unsigned char *d_image,
        int m2 = iter == EVEN ? (val[5] * val[7] * val[1])
           : (val[3] * val[5] * val[1]);
        if (A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0) {
-          d_marker[offset] = 1;
           marker = 1;
        } else {
-          d_marker[offset] = 0;
           marker = 0;
        }
     }
     __syncthreads();
     
     if (offset < width * height) {
-       // d_image[offset] &= ~d_marker[offset];
        d_image[offset] &= ~marker;
     }
+}
+
+__global__
+void cuAbsDiff(int *value, unsigned char *d_image, unsigned char *d_prev,
+               const int width, const int height) {
+    int t_idx = threadIdx.x + blockIdx.x * blockDim.x;
+    int t_idy = threadIdx.y + blockIdx.y * blockDim.y;
+    int offset = t_idx + t_idy * blockDim.x * gridDim.x;
+
+    if (offset == 0) {
+       *value = 0;
+    }
+    __syncthreads();
     
-    /*
+    if (offset < width * height) {
+       if (abs(d_image[offset] - d_prev[offset]) > 0) {
+          atomicAdd(value, 1);
+       }
+    }
     __syncthreads();
 
-    if ((t_idx > 0 && t_idx < width - 1) &&
-        (t_idy > 0 && t_idy < height - 1)) {
-       d_marker[offset] = 0.0f;
+    if (offset < width * height) {
+       d_prev[offset] = d_image[offset];
     }
-    */
 }
 
 void skeletonizationGPU(cv::Mat image) {
@@ -102,36 +114,56 @@ void skeletonizationGPU(cv::Mat image) {
     cudaMalloc(reinterpret_cast<void**>(&d_image), dev_mal);
     cudaMemcpy(d_image, data, dev_mal, cudaMemcpyHostToDevice);
 
-    unsigned char *d_marker;
-    cudaMalloc(reinterpret_cast<void**>(&d_marker), dev_mal);
-    cudaMemset(d_marker, 0, dev_mal);
+    unsigned char *d_prev;
+    cudaMalloc(reinterpret_cast<void**>(&d_prev), dev_mal);
+    cudaMemcpy(d_prev, prev_data, dev_mal, cudaMemcpyHostToDevice);
 
+    
+    int *d_count;
+    cudaMalloc(reinterpret_cast<void**>(&d_count), sizeof(int));
+    
     bool is_zero = true;
     unsigned char temp_data[im_size];
     int icounter = 0;
+
+    int value;
     do {
        skeletonizationKernel<<<grid_size, block_size>>>(
-          d_marker, d_image, 0, image.cols, image.rows);
+          d_image, 0, image.cols, image.rows);
        cudaDeviceSynchronize();
        
        skeletonizationKernel<<<grid_size, block_size>>>(
-          d_marker, d_image, 1, image.cols, image.rows);
+          d_image, 1, image.cols, image.rows);
        cudaDeviceSynchronize();
-
-       cudaMemcpy(temp_data, d_image, dev_mal, cudaMemcpyDeviceToHost);
+       
+       // cudaMemset(d_count, 0, sizeof(int));
+       cuAbsDiff<<<grid_size, block_size>>>(
+          d_count, d_image, d_prev, image.cols, image.rows);
+       cudaMemcpy(&value, d_count, sizeof(int), cudaMemcpyDeviceToHost);
 
        is_zero = true;
+       if (value > 0) {
+          is_zero = false;
+       } else {
+          cudaMemcpy(temp_data, d_image, dev_mal, cudaMemcpyDeviceToHost);
+       }
+
+       
+       
+       /*
+       cudaMemcpy(temp_data, d_image, dev_mal, cudaMemcpyDeviceToHost);
        for (int i = 0; i < im_size; i++) {
           if (std::abs(temp_data[i] - prev_data[i]) > 0) {
              is_zero = false;
           }
           prev_data[i] = temp_data[i];
        }
-
+       */
        if (icounter++ == 1000) {
           is_zero = true;
        }
-       std::cout << "ITERATING..."  << icounter << "\n";
+       
+       std::cout << "ITERATING..."  << icounter  << "\t" << value << "\n";
        
     } while (!is_zero);
     
